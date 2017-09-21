598406c320214316bb0d91c45bfe150ebbfd26fb
Graeme MacAdam <Graeme.MacAdam@Microtech.local>
2017-09-21 11:12:49 +0100

    android files

---
+++ app/src/main/java/microtech_group/co/uk/testpinscreen/MainActivity.java
@@ -0,0 +1,22 @@
+package microtech_group.co.uk.testpinscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }*/
+}
